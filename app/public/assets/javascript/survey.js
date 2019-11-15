03289bf7ec7b5b7c24ef147fc3f9489d9fe28394
Haakasaurus <kramerhjohnson@gmail.com>
2019-11-15 15:07:17 -0800

    added code to survey.js to return object of user data when submit is clicked.

--- app/public/assets/javascript/survey.js
+++ app/public/assets/javascript/survey.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
     $('select').formSelect();
+
+    let user = {};
+
+    $("#submit-btn").on("click", () => {
+        user.name = $("#name-input").val();
+        user.photo = $("#photo-input").val();
+        let scores = [];
+        for (let i = 1; i <= 10; i++) {
+            scores.push(parseInt($("#q-" + i).val()));
+        }
+        user.scores = scores;
+
+        console.log(JSON.stringify(user));
+    })
 });
+
+
