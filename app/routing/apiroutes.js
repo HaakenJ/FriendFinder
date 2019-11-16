d052c59bad2fe7b1eaad960901106bcd9c894f1d
Haakasaurus <kramerhjohnson@gmail.com>
2019-11-16 09:41:02 -0800

    Fixed bugs in findClosestMatch()

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -11,11 +11,9 @@ app.use(express.json());
 router.post("/api/users", (req, res) => {
     const newUser = req.body;
 
-
-
     friends.push(newUser);
 
-    res.json(newUser);
+    res.json(findClosestMatch(friends, newUser));
 })
 
 router.get("/api/users", (req, res) => {
@@ -25,16 +23,6 @@ router.get("/api/users", (req, res) => {
 
 module.exports = router;
 
-// get new user data
-// loop through friends list
-// for each friend, loop through the friend's scores
-// for each number in scores, find the diff between newUser's score at that same index
-// add the absolute value of the difference to total score
-// if total score is greater than closestMatch's total score,
-// store the totalScore in highestScore
-// store the current friend in closestMatch
-// At the end of the loop of friends, return the closest match friend
-
 function findClosestMatch(friendList, user) {
     let totalScore = 0,
         closestMatch,
@@ -42,14 +30,21 @@ function findClosestMatch(friendList, user) {
         diff = 0;
 
     friendList.forEach(friend => {
+        totalScore = 0;
         for (let i = 0; i < friend.scores.length; i++) {
             diff = friend.scores[i] - user.scores[i];
             totalScore += Math.abs(diff);
         }
-        if (totalScore > highScore) {
+        if (highScore === 0 && closestMatch === undefined) {
+            highScore = totalScore;
+            closestMatch = friend;
+            
+        } else if (totalScore < highScore) {
             highScore = totalScore;
             closestMatch = friend;
         }
     })
     return closestMatch;
 }
+
+
