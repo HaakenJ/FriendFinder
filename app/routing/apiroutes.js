50bcde6dd038dcb5d5dff8bdaa0078273e069061
Haakasaurus <kramerhjohnson@gmail.com>
2019-11-15 21:36:48 -0800

    Added routing for api get request of friends list.  Data is lost when server goes down though.

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -11,8 +11,6 @@ app.use(express.json());
 router.post("/api/users", (req, res) => {
     const newUser = req.body;
 
-    newUser.routeName = newUser.name.replace(/\s+/g, "").toLowerCase();
-
     friends.push(newUser);
 
     res.json(newUser);
