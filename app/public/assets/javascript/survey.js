8cf7375fc3fbf89e77b5d33e1b72c000572e60a8
Haakasaurus <kramerhjohnson@gmail.com>
2019-11-17 19:58:02 -0800

    Added user's gender choice to survey.js

--- app/public/assets/javascript/survey.js
+++ app/public/assets/javascript/survey.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
         user.name = $("#name-input").val().trim();
         user.photo = $("#photo-input").val().trim();
         user.gender = $("#user-gender").val().trim();
+        user.preference = $("#gender-choice").val().trim();
         // To-do: specify code for user's choice of gender
         let scores = [];
         for (let i = 1; i <= 10; i++) {
