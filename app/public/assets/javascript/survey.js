f0e3a5bedf0842916463c60d7e9d3b905381e1ad
Haakasaurus <kramerhjohnson@gmail.com>
2019-11-17 20:44:43 -0800

    Added modals to be displayed if a user doesn't enter certain info.

--- app/public/assets/javascript/survey.js
+++ app/public/assets/javascript/survey.js
@@ -7,23 +7,38 @@ $(document).ready(function () {
     $("#submit-btn").on("click", () => {
         user.name = $("#name-input").val().trim();
         user.photo = $("#photo-input").val().trim();
+
+        if ($("#gender-choice").val() === null) {
+            $("#gender-modal").modal("open");
+            return;
+        }
         user.preference = $("#gender-choice").val().trim();
+
         let scores = [];
         for (let i = 1; i <= 10; i++) {
-            scores.push(parseInt($("#q-" + i).val()));
+            if (isNaN(parseInt($("#q-" + i).val()))) {
+                scores.push(0);
+            } else {
+                scores.push(parseInt($("#q-" + i).val()));
+            }
         }
         user.scores = scores;
 
+        if (user.name === "" || user.photo === "") {
+            $("#name-url-modal").modal("open");
+            return;
+        }
+
+        console.log(user);
+
         $.post("/api/users", user)
-        .then((data) => {
-            console.log(data);
-            $("#match-name").text(data.name);
-            $("#match-img").attr("src", data.photo);
-            $('#modal1').modal('open'); 
-        });
+            .then((data) => {
+                console.log(data);
+                $("#match-name").text(data.name);
+                $("#match-img").attr("src", data.photo);
+                $('#match-modal').modal("open");
+            });
 
         console.log(JSON.stringify(user));
     })
 });
-
-
